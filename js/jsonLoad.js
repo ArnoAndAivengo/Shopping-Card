59efac68a750ac3d88f546343b6207eb196d2836
ArnoAndAivengo <alexobukhov@hotmail.com>
2018-12-10 13:26:43 +0300

    add

--- js/jsonLoad.js
+++ js/jsonLoad.js
@@ -9,7 +9,7 @@ fetch(url)
     let items = document.querySelectorAll('.items');
 
     $.each(body , function (key){
-            items[0].innerHTML = '<p>' + "key" + '</p>' + '<p>' + 'Price:$' + body.hdd + '</p>';
+            items[0].innerHTML = '<p>' + "hdd" + '</p>' + '<p>' + 'Price:$' + body.hdd + '</p>';
             items[1].innerHTML = '<p>' + "sdd" + '</p>' + '<p>' + 'Price:$' + body.sdd + '</p>';
             items[2].innerHTML = '<p>' + "usbdrive" + '</p>' + '<p>' + 'Price:$' + body.usbdrive + '</p>';
     });
